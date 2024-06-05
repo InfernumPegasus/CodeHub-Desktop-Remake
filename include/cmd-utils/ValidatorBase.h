24f83886da7125096b9eda130f0cb7a534c7fd75
Vladimir Kirillov <vkirillov505@gmail.com>
2024-06-06 00:31:23 +0300

    Template `Status` parameter added

--- include/cmd-utils/ValidatorBase.h
+++ include/cmd-utils/ValidatorBase.h
@@ -6,14 +6,14 @@
 
 namespace codehub::utils {
 
-template <class T>
+template <class Object, class Status>
 class ValidatorBase {
  public:
   virtual ~ValidatorBase() = default;
 
   void SetNext(std::shared_ptr<ValidatorBase> next) { m_next = std::move(next); }
 
-  virtual bool Validate(const T&) = 0;
+  virtual Status Validate(const Object&) = 0;
 
  protected:
   std::shared_ptr<ValidatorBase> m_next;
