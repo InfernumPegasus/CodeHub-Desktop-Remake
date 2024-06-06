a491ab23199ee0cb4970e5bb50f86058f9289fb3
Vladimir Kirillov <vkirillov505@gmail.com>
2024-06-07 00:29:59 +0300

    `PrintCommandLineParserStatusError` function added

--- main.cpp
+++ main.cpp
@@ -6,6 +6,25 @@
 
 using namespace codehub::utils;
 
+// TODO : move into logging module
+void PrintCommandLineParserStatusError(
+    CommandLineParser::CommandLineParserStatus status) {
+  switch (status) {
+    case CommandLineParser::CommandLineParserStatus::UNKNOWN_COMMAND_KEYWORD:
+      inferlib::Printer::Println(std::cerr, "Error: No such command");
+      break;
+    case CommandLineParser::CommandLineParserStatus::WRONG_KEYWORD:
+      inferlib::Printer::Println(std::cerr, "Error: Wrong command keyword format");
+      break;
+    case CommandLineParser::CommandLineParserStatus::WRONG_FLAG:
+      inferlib::Printer::Println(std::cerr, "Error: Wrong command flag or value format");
+      break;
+    default:
+      inferlib::Printer::Println(std::cerr, "Error: Unknown error occurred");
+      break;
+  }
+}
+
 int main(int argc, char* argv[]) {
   CommandLineParser parser;
   auto result = parser.Parse(argc, argv);
@@ -14,17 +33,10 @@ int main(int argc, char* argv[]) {
     auto command = result.value();
     inferlib::Printer::Println(std::cout, command);
 
-    constexpr std::array<std::pair<std::string_view, CommandVariant>,
-                         std::variant_size_v<CommandVariant>>
-        commands{{
-            {"help", HelpCommand()},
-            {"add", AddCommand()},
-        }};
-
-    constexpr CommandRegistry commandRegistry{{commands}};
-
-    CommandExecutor executor(commandRegistry);
-    executor.ExecuteCommand(command.value());
+    CommandExecutor::ExecuteCommand(command);
+  } else {
+    auto errorStatus = result.error();
+    PrintCommandLineParserStatusError(errorStatus);
   }
 
   return 0;
