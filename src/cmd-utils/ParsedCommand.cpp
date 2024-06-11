b39b5602416e665b7b8f1d7f8bed16c1f2261894
Vladimir Kirillov <vkirillov505@gmail.com>
2024-06-11 17:03:44 +0300

    Outputs renamed

--- src/cmd-utils/ParsedCommand.cpp
+++ src/cmd-utils/ParsedCommand.cpp
@@ -5,14 +5,14 @@ namespace codehub::utils {
 std::ostream& operator<<(std::ostream& os, const ParsedCommand& command) {
   os << "Keyword: " << command.m_keyword << "\nFlags and their arguments:\n";
   for (const auto& [flag, hasValue] : command.m_flags) {
-    os << "ParsedCommandFlag: " << flag.first;
+    os << "Command flag: " << flag.first;
     if (hasValue && !flag.second->empty()) {
       os << " | Argument: " << flag.second.value();
     }
     os << '\n';
   }
 
-  os << "Simple Arguments: ";
+  os << "Simple arguments: ";
   for (const auto& arg : command.m_args) {
     os << arg << ' ';
   }
