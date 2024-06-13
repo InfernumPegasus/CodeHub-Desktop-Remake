7bcc921eb5810b3d00db0b45dbbbfbca8eaf5452
Vladimir Kirillov <vkirillov505@gmail.com>
2024-06-13 23:06:45 +0300

    Version info constants

---
+++ include/utils/config/VersionInfo.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string_view>
+
+namespace codehub::version {
+
+static constexpr std::string_view ABOUT{"CodeHub Remake is a small git-like VCS."};
+
+static constexpr std::string_view VERSION{"0.1"};
+
+static constexpr std::string_view AUTHOR{"Vladimir Kirillov"};
+
+static constexpr std::string_view AUTHOR_GITHUB{"https://github.com/InfernumPegasus"};
+
+}  // namespace codehub::version
