04c2d8ad8a0fc3c8c6a448264b3f824ebc84dd4e
Vladimir Kirillov <vkirillov505@gmail.com>
2024-07-03 16:00:58 +0300

    `[[nodiscard]]` attribute added, `IValidator` import removed

--- include/utils/parser/CommandLineParser.h
+++ include/utils/parser/CommandLineParser.h
@@ -3,21 +3,23 @@
 #include <expected>
 #include <memory>
 
-#include "IValidator.h"
 #include "ParsedCommand.h"
 #include "utils/error/Error.h"
 
 namespace codehub::utils {
 
 struct CommandLineParser {
-  static ParsedCommand Parse(int argc, char* argv[]);
+  [[nodiscard]] static ParsedCommand Parse(int argc, char* argv[]);
 
  private:
-  static constexpr CommandArgsList ArgvToStringViews(int argc, char* argv[]);
+  [[nodiscard]] static constexpr CommandArgsList ArgvToStringViews(int argc,
+                                                                   char* argv[]);
 
-  static constexpr CommandFlagsList ExtractFlagsWithArgs(const CommandArgsList& rawArgs);
+  [[nodiscard]] static constexpr CommandFlagsList ExtractFlagsWithArgs(
+      const CommandArgsList& rawArgs);
 
-  static constexpr CommandArgsList ExtractSimpleArgs(const CommandArgsList& rawArgs);
+  [[nodiscard]] static constexpr CommandArgsList ExtractSimpleArgs(
+      const CommandArgsList& rawArgs);
 };
 
 }  // namespace codehub::utils
