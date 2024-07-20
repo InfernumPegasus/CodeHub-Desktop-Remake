7d73114fc838825d93308ee86d71669ba347f596
Vladimir Kirillov <vkirillov505@gmail.com>
2024-07-20 17:35:18 +0300

    Readability improved

--- include/command/utils/CommandsUtils.h
+++ include/command/utils/CommandsUtils.h
@@ -20,26 +20,26 @@ using CommandVariant = std::variant<
     CheckoutCommand
 >;
 
-constexpr std::string_view HELP_COMMAND = "help";
-constexpr std::string_view CONFIG_COMMAND = "config";
-constexpr std::string_view INIT_COMMAND = "init";
-constexpr std::string_view ADD_COMMAND = "add";
-constexpr std::string_view COMMIT_COMMAND = "commit";
-constexpr std::string_view REVERT_COMMAND = "revert";
-constexpr std::string_view LOG_COMMAND = "log";
-constexpr std::string_view VERSION_COMMAND = "version";
-constexpr std::string_view BRANCH_COMMAND = "branch";
-constexpr std::string_view CHECKOUT_COMMAND = "checkout";
+constexpr std::string_view HELP_COMMAND         = "help";
+constexpr std::string_view CONFIG_COMMAND       = "config";
+constexpr std::string_view INIT_COMMAND         = "init";
+constexpr std::string_view ADD_COMMAND          = "add";
+constexpr std::string_view COMMIT_COMMAND       = "commit";
+constexpr std::string_view REVERT_COMMAND       = "revert";
+constexpr std::string_view LOG_COMMAND          = "log";
+constexpr std::string_view VERSION_COMMAND      = "version";
+constexpr std::string_view BRANCH_COMMAND       = "branch";
+constexpr std::string_view CHECKOUT_COMMAND     = "checkout";
 
-constexpr std::string_view HELP_DESCRIPTION = "List of all supported commands";
-constexpr std::string_view CONFIG_DESCRIPTION = "Configure global settings";
-constexpr std::string_view INIT_DESCRIPTION = "Init repository";
-constexpr std::string_view ADD_DESCRIPTION = "Add file(s), directory(ies) to tracked";
-constexpr std::string_view COMMIT_DESCRIPTION = "Commit added files";
-constexpr std::string_view REVERT_DESCRIPTION = "Revert certain commit";
-constexpr std::string_view LOG_DESCRIPTION = "See commit log in branch";
-constexpr std::string_view VERSION_DESCRIPTION = "Version of software";
-constexpr std::string_view BRANCH_DESCRIPTION = "Create new branch";
+constexpr std::string_view HELP_DESCRIPTION     = "List of all supported commands";
+constexpr std::string_view CONFIG_DESCRIPTION   = "Configure global settings";
+constexpr std::string_view INIT_DESCRIPTION     = "Init repository";
+constexpr std::string_view ADD_DESCRIPTION      = "Add file(s), directory(ies) to tracked";
+constexpr std::string_view COMMIT_DESCRIPTION   = "Commit added files";
+constexpr std::string_view REVERT_DESCRIPTION   = "Revert certain commit";
+constexpr std::string_view LOG_DESCRIPTION      = "See commit log in branch";
+constexpr std::string_view VERSION_DESCRIPTION  = "Version of software";
+constexpr std::string_view BRANCH_DESCRIPTION   = "Create new branch";
 constexpr std::string_view CHECKOUT_DESCRIPTION = "Change branch";
 
 static constexpr auto COMMAND_DESCRIPTIONS =
@@ -69,7 +69,6 @@ static constexpr auto GLOBAL_COMMAND_REGISTRY =
         std::pair{BRANCH_COMMAND, BranchCommand()},
         std::pair{CHECKOUT_COMMAND, CheckoutCommand()}
     );
-
 // clang-format on
 
 static_assert(inferlib::AllEqual(GLOBAL_COMMAND_REGISTRY.data.size(),
