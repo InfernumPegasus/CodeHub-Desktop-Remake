a0c531886e0a24bfc156edde470db7df83d39be0
Vladimir Kirillov <vkirillov505@gmail.com>
2024-09-15 01:49:07 +0300

    `operator<<` removed

--- include/utils/parser/ParsedCommand.h
+++ include/utils/parser/ParsedCommand.h
@@ -4,12 +4,12 @@
 
 namespace codehub::utils {
 
+/// Класс распаршенной команды с ключевым словом, флагами и их параметрами
+/// и простыми аргументами
 struct ParsedCommand {
   std::string_view m_keyword;
   FlagsListView m_flags;
   ArgsListView m_args;
-
-  friend std::ostream& operator<<(std::ostream& os, const ParsedCommand& command);
 };
 
 }
