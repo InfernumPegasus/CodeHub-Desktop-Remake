e721b15ae112a43c456aecbd211d5f10f0bf5050
Vladimir Kirillov <vkirillov505@gmail.com>
2024-09-15 01:47:05 +0300

    `ExecuteImpl` prototype added

--- src/command/ConfigCommand.cpp
+++ src/command/ConfigCommand.cpp
@@ -1,26 +1,52 @@
 #include "command/ConfigCommand.h"
 
+#include <fstream>
+
 #include "lib/inferlib/printer/Printer.h"
 #include "utils/config/ConfigFilenames.h"
+#include "utils/config/IniConfigWriter.h"
+#include "utils/parser/ParsedCommand.h"
 #include "utils/status/Status.h"
 
 namespace codehub::utils {
 
 CommandExecutionStatus ConfigCommand::ExecuteImpl(const ParsedCommand& command) {
+  inferlib::Printer::Println(std::cout, "ConfigCommand is executing!");
+
+  GlobalAppConfig config("yui", "vbn");
+  if (!WriteGlobalAppConfig(config)) {
+    return CommandExecutionStatus::RUNTIME_ERROR;
+  }
+
+  return CommandExecutionStatus::OK;
+}
+
+bool ConfigCommand::CreateGlobalAppConfig() {
   using namespace codehub::filesystem;
 
   if (!ElementExists(GLOBAL_APP_FOLDER_PATH) &&
       !CreateDirectory(GLOBAL_APP_FOLDER_PATH)) {
-    return CommandExecutionStatus::RUNTIME_ERROR;
+    return false;
   }
 
   if (!ElementExists(GLOBAL_APP_CONFIG_PATH) && !CreateFile(GLOBAL_APP_CONFIG_PATH)) {
-    return CommandExecutionStatus::RUNTIME_ERROR;
+    return false;
   }
 
-  inferlib::Printer::Println(std::cout, "ConfigCommand is executing!");
+  return true;
+}
 
-  return CommandExecutionStatus::OK;
+bool ConfigCommand::WriteGlobalAppConfig(const utils::GlobalAppConfig& config) {
+  using namespace codehub::filesystem;
+
+  std::ofstream configFile(GLOBAL_APP_CONFIG_PATH);
+  if (!configFile.is_open()) {
+    return false;
+  }
+
+  IniConfigWriter::Write(configFile, config);
+
+  return true;
 }
 
 }  // namespace codehub::utils
