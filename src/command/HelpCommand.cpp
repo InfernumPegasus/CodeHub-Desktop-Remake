dafe37d022d5ecfc0e1329d6a41d536773222d36
Vladimir Kirillov <vkirillov505@gmail.com>
2024-09-15 01:47:32 +0300

    `command` namespace used

--- src/command/HelpCommand.cpp
+++ src/command/HelpCommand.cpp
@@ -2,25 +2,25 @@
 
 #include <iomanip>
 
-#include "command/utils/CommandsUtils.h"
+#include "command/CommandsUtils.h"
 #include "lib/inferlib/printer/Printer.h"
 
 namespace codehub::utils {
 
 CommandExecutionStatus HelpCommand::ExecuteImpl(const ParsedCommand&) {
   using namespace inferlib;
+  using namespace command;
 
   constexpr size_t maxNameLength =
       std::max_element(
-          command::COMMAND_DESCRIPTIONS.data.begin(),
-          command::COMMAND_DESCRIPTIONS.data.end(),
+          COMMAND_DESCRIPTIONS.data.cbegin(), COMMAND_DESCRIPTIONS.data.cend(),
           [](const auto& a, const auto& b) { return a.first.size() < b.first.size(); })
           ->first.size();
 
   Printer::Println(std::cout, "Available commands:");
-  for (const auto& [name, description] : command::COMMAND_DESCRIPTIONS.data) {
+  for (const auto& [name, description] : COMMAND_DESCRIPTIONS.data) {
     Printer::Println(std::cout, name,
-                     std::setw(static_cast<int>(maxNameLength - name.size() + 2)), " - ",
+                     std::setw(static_cast<int>(maxNameLength - name.size() + 3)), " - ",
                      description);
   }
 
