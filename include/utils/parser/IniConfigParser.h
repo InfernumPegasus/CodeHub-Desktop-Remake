35543c4fb8d38dd706d90efb45d8ad8929c10f2b
Vladimir Kirillov <vkirillov505@gmail.com>
2024-09-17 21:47:24 +0300

    Key symbols specified

--- include/utils/parser/IniConfigParser.h
+++ include/utils/parser/IniConfigParser.h
@@ -10,10 +10,17 @@ namespace codehub::utils {
 
 /// Класс для унифицированного парсинга ini-файлов
 struct IniConfigParser {
+  static constexpr char32_t COMMENT_SYMBOL = U';';
+
+  static constexpr char32_t OPEN_SECTION_SYMBOL = U'[';
+
+  static constexpr char32_t CLOSE_SECTION_SYMBOL = U']';
+
+  static constexpr char32_t VALUE_DELIMITER_SYMBOL = U'=';
 
   /// Метод для парсинга ini-файла из считанного файла
   /// \param lines строки считанного файла
-  /// \return готовый объект ini-файла
+  /// \return заполненный объект ini-файла
   [[nodiscard]] static ParsedIniFile Parse(const std::vector<std::string>& lines);
 };
 
--- src/utils/parser/IniConfigParser.cpp
+++ src/utils/parser/IniConfigParser.cpp
@@ -10,28 +10,28 @@ ParsedIniFile IniConfigParser::Parse(const std::vector<std::string>& lines) {
   std::string currentSection;
 
   for (const auto& line : lines) {
-    if (line.empty() || line.front() == ';' || line.front() == '#') {
+    if (line.empty() || line.front() == COMMENT_SYMBOL) {
       continue;
     }
 
-    if (line.front() == '[' && line.back() == ']') {
+    if (line.front() == OPEN_SECTION_SYMBOL && line.back() == CLOSE_SECTION_SYMBOL) {
       currentSection = line.substr(1, line.size() - 2);
       iniMap[currentSection] = FlagsList();
       continue;
     }
 
-    const auto pos = line.find('=');
+    const auto pos = line.find(VALUE_DELIMITER_SYMBOL);
     if (pos != std::string::npos) {
       const auto key = line.substr(0, pos);
       auto value = line.substr(pos + 1);
       const bool shouldHaveValue = !value.empty();
 
-      const auto commentPos = value.find(';');
+      const auto commentPos = value.find(COMMENT_SYMBOL);
       if (commentPos != std::string::npos) {
         value = value.substr(0, commentPos);
       }
 
-      const ParsedFlag flag = {
+      const ParsedFlag flag{
           {key, shouldHaveValue ? std::make_optional<std::string>(value) : std::nullopt},
           shouldHaveValue};
       iniMap[currentSection].push_back(flag);
