ddab656e79478d3ee389d5bfdfaaf9a044024cd8
Vladimir Kirillov <vkirillov505@gmail.com>
2024-09-22 13:34:21 +0300

    Methods signature changed

--- include/utils/config/GlobalAppConfig.h
+++ include/utils/config/GlobalAppConfig.h
@@ -8,16 +8,17 @@ namespace codehub::utils {
 
 /// Отражает ini-файл в секции [GlobalAppConfig].
 /// Включает в себя поля 'username' и 'email'
-struct GlobalAppConfig {
+class GlobalAppConfig {
+ public:
   static constexpr std::string_view SectionName = "GlobalAppConfig";
 
   GlobalAppConfig(const std::string& username, const std::string& email);
 
-  GlobalAppConfig() = default;
+  constexpr GlobalAppConfig() = default;
 
-  [[nodiscard]] const std::string& GetUsername() const;
+  [[nodiscard]] std::string_view GetUsername() const;
 
-  [[nodiscard]] const std::string& GetEmail() const;
+  [[nodiscard]] std::string_view GetEmail() const;
 
  private:
   std::string m_username;
--- src/utils/config/GlobalAppConfig.cpp
+++ src/utils/config/GlobalAppConfig.cpp
@@ -2,9 +2,9 @@
 
 namespace codehub::utils {
 
-const std::string& GlobalAppConfig::GetUsername() const { return m_username; }
+std::string_view GlobalAppConfig::GetUsername() const { return m_username; }
 
-const std::string& GlobalAppConfig::GetEmail() const { return m_email; }
+std::string_view GlobalAppConfig::GetEmail() const { return m_email; }
 
 GlobalAppConfig::GlobalAppConfig(const std::string& username, const std::string& email)
     : m_username(username), m_email(email) {}
